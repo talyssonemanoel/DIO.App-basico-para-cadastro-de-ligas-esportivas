15f90fad8d302bb4387495eaaf993ce23e8d4b55
talyssonemanoel <90127158+talyssonemanoel@users.noreply.github.com>
2021-09-14 14:59:50 -0300

    Create Modalidade.cs

---
+++ Enum/Modalidade.cs
@@ -0,0 +1,21 @@
+namespace DIO.Ligas
+{
+	public enum Modalidade
+	{
+		Atletismo = 1,
+		Basquete = 2,
+		Boxe = 3,
+		Futebol = 4,
+		FutebolAmericano = 5,
+		Futsal = 6,
+		GinasticaArtistica = 7,
+		Judo = 8,
+		MMA = 9,
+		Motor = 10,
+		Natacao = 11,
+		Skate = 12,
+		Surfe = 13,
+		Tenis = 14,
+		Volei = 15,
+	}
+}
